72174a30bfde4c00d4a884c52e78991ba9f9fd87
Venkat Tata <saikrish1477@gmail.com>
2021-10-17 18:44:39 -0600

    Assignment-7-complete

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,9 +29,34 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
 			size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+	//Counter to break out of while loop when requested entry not found
+    int counter=0;
+    
+    //Location to start searching from the first entry
+    int buffer_pos = buffer->out_offs;
+    
+    //Iterate based on the number of entries permissible
+    while(counter<AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+    {
+		//char offset less than size of entry, the requested char must be in this entry
+		if(char_offset < buffer->entry[buffer_pos].size)
+		{
+			//The offset location is same as the updated char offset 
+			//and return the entry number
+			*entry_offset_byte_rtn = char_offset;
+			return &(buffer->entry[buffer_pos]);
+		}
+		
+		//If not found , increment the counter
+		counter++;
+		
+		//Update the offset by decrementing the size of entry that did not have the character requested
+		char_offset = char_offset- buffer->entry[buffer_pos].size;
+		
+		//Updated buffer position to next entry in the buffer and wrap around after reaching end
+		buffer_pos = (buffer_pos +1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+    //If requested character not found, returns NULL
     return NULL;
 }
 
@@ -44,9 +69,33 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description 
-    */
+	//Check if the buffer is not NULL
+	if(buffer == NULL)
+		return;
+	
+	//Store the buffptr and its size into circular buffer
+	buffer->entry[buffer->in_offs] = *add_entry;
+	
+	//Initially "in" equals "out",yet the buffer is not full and not set as full. After the first entry, every time
+	//"in" equals "out" the buffer is set to full. Once the buffer is full, the "out" is incremented by 1 and on
+	//reaching the end of circular buffer, just like the "in" , even the "out" is wrapped around
+	if(buffer->out_offs != buffer->in_offs)
+    {
+		buffer->full = false;
+    }
+    else
+    {
+		if(buffer->initial == false)
+		{
+			buffer->full = true;
+			buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+		}
+		else
+			buffer->initial=false;
+	}
+	
+	//Increment the buffer position and wrap around if end of buffer is reached
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
 }
 
 /**
@@ -55,4 +104,5 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
 void aesd_circular_buffer_init(struct aesd_circular_buffer *buffer)
 {
     memset(buffer,0,sizeof(struct aesd_circular_buffer));
+    buffer->initial=true;
 }
